290cb5fc07042c87a75d598bb31759119ae04d3e
debas-31 <debas31@gmail.com>
2022-08-16 10:59:23 +0300

    Add signin form

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,3 +1,34 @@
-const SignInPage = () => <h1>This is SignIn page</h1>;
+import React from 'react';
+
+const SignInPage = () => (
+  <>
+    <div className="container d-flex justify-content-center align-items-center m-5">
+      <form className="form">
+        <h3>
+          LogIn
+        </h3>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="username"
+            placeholder="User name"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        <button className="btn btn-success" type="submit">
+          LogIn
+        </button>
+      </form>
+    </div>
+  </>
+);
 
 export default SignInPage;
