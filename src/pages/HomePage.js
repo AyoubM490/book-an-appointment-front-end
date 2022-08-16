16358d78fcafe6de52c47b6eb540807fe1d1d60a
debas-31 <debas31@gmail.com>
2022-08-16 10:57:05 +0300

    Modify Home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 
 const HomePage = () => (
 
   <>
     <div className="container d-flex flex-column justify-content-center align-items-center mt-5">
       <div className="col ">
-        <Link to="/signin"><Button type="button" className="btn info m-2">LOGIN</Button></Link>
+        <Link to="/signin"><button type="button" className="btn btn-success m-2">LOGIN</button></Link>
       </div>
       <div className="col ">
-        <Link to="/signup"><Button type="button" className="btn info m-2">SIGNUP</Button></Link>
+        <Link to="/signup"><button type="button" className="btn btn-success m-2">SIGNUP</button></Link>
       </div>
     </div>
   </>
