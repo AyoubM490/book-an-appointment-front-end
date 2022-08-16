17852177acff65996f35feb345f492be5cbc5335
debas-31 <debas31@gmail.com>
2022-08-16 10:58:59 +0300

    Add signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,3 +1,44 @@
-const SignUpPage = () => <h1>This is SignUp page</h1>;
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const SignUpPage = () => (
+  <>
+    <div className="container col-sm-4 ">
+      <FontAwesomeIcon icon="fa-regular fa-xmark" />
+      <form className="form m-2">
+        <h3 className="text-center">
+          SignUp
+        </h3>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="username"
+            placeholder="User name"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="password"
+            name="password2"
+            placeholder="Confirm password"
+          />
+        </div>
+        <button className="btn btn-success" type="submit">
+          Sign up
+        </button>
+      </form>
+    </div>
+  </>
+);
 
 export default SignUpPage;
