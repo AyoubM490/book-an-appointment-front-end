adf45876d6d0030e060dc35c4a8670c4c1ff3ea0
debas-31 <debas31@gmail.com>
2022-08-17 12:27:34 +0300

    Add social-media icons

--- src/layout/navigation/Sidebar.js
+++ src/layout/navigation/Sidebar.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './Navigation.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faYoutube,
+    faFacebook,
+    faTwitter,
+    faInstagram,
+    faGooglePlus
+  } from "@fortawesome/free-brands-svg-icons";
 
 const Navigation = () => (
   <nav className="sidenav">
@@ -17,6 +25,39 @@ const Navigation = () => (
         <NavLink to="#" activeClassName="active" className="nav-link">RESERVATIONS</NavLink>
       </div>
     </div>
+    <div className="d-flex flex-column align-items-center justify-content-center ">
+      <p className="social-wraper">
+      <a href="https://wwww.twitter.com" className="twitter social text-black-50 px-1">
+          <FontAwesomeIcon icon={faTwitter} fontSize='23px' />
+        </a>
+        <a
+          href="https://www.facebook.com"
+          className="facebook social text-black-50 px-1"
+        >
+          <FontAwesomeIcon icon={faFacebook} fontSize='23px' />
+        </a>
+        <a
+          href="https://www.google.com"
+          className="googleplus social text-black-50 px-1"
+        >
+          <FontAwesomeIcon icon={faGooglePlus} fontSize='23px' />
+        </a>
+        <a
+          href="https://www.youtube.com"
+          className="youtube social text-black-50 px-1"
+        >
+          <FontAwesomeIcon icon={faYoutube} fontSize='23px' />
+        </a>
+       
+        <a
+          href="http://www.instagram.com"
+          className="instagram social text-black-50 px-1"
+        >
+          <FontAwesomeIcon icon={faInstagram} fontSize='23px' />
+        </a>
+      </p>
+      <div> &copy; 2022 Power Motors Inc.</div>
+      </div>
   </nav>
 );
 
