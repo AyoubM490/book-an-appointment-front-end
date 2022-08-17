c601c52bf1332036247ac2aa256d4a602165af8f
debas-31 <debas31@gmail.com>
2022-08-17 08:32:40 +0300

    Add api to add, show, update and delete a motorcyle

--- src/redux/api.js
+++ src/redux/api.js
@@ -16,11 +16,60 @@ const API = () => {
       });
   },
 
-  register; (user, success) => {
+  register: (user, success) => {
     axios.post(`${baseURL}/users`,
       {
         user,
       })
+      .then((response) => {
+        success(response);
+      })
+      .catch((error) => {
+        success(error.message);
+      });
+  },
+  fetchMotors: (success) => {
+    axios.get(`${baseURL}/motocycles`)
+      .then((response) => {
+        success(response);
+      })
+      .catch((error) => {
+        success(error.message);
+      });
+  },
+  fetchSingleMotor: (id, success) => {
+    axios.get(`${baseURL}/motocycles/${id}`)
+      .then((response) => {
+        success(response);
+      })
+      .catch((error) => {
+        success(error.message);
+      });
+  },
+  addMotor: (motor, success) => {
+    axios.post(`${baseURL}/motocycles`, {
+      motors: motor,
+    })
+      .then((response) => {
+        success(response);
+      })
+      .catch((error) => {
+        success(error.message);
+      });
+  },
+  updateMotor: (id, motor, success) => {
+    axios.post(`${baseURL}/motocycles/${id}`, {
+      motors: motor,
+    })
+      .then((response) => {
+        success(response);
+      })
+      .catch((error) => {
+        success(error.message);
+      });
+  },
+  deleteMotor: (id, success) => {
+    axios.delete(`${baseURL}/motocycles/${id}`)
       .then((response) => {
         success(response);
       })
