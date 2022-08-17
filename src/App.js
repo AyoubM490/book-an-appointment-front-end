c91443b2b7bc8df7cbd330de7effa2a632ae653d
debas-31 <debas31@gmail.com>
2022-08-17 13:07:10 +0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './layout/navigation/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/layout/navigation/Sidebar.js
+++ src/layout/navigation/Sidebar.js
@@ -3,12 +3,12 @@ import { NavLink, Link } from 'react-router-dom';
 import './Navigation.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faYoutube,
-    faFacebook,
-    faTwitter,
-    faInstagram,
-    faGooglePlus
-  } from "@fortawesome/free-brands-svg-icons";
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faGooglePlus,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Navigation = () => (
   <nav className="sidenav">
@@ -18,46 +18,46 @@ const Navigation = () => (
           <h1 className="text-center text-dark"><i>Power Motors</i></h1>
         </Link>
       </div>
-      <div className="ml-2 d-flex flex-column align-items-center">
-        <NavLink to="#" exact activeClassName="active" className="nav-link ">MOTORSCYCLES</NavLink>
-        <NavLink to="#" activeClassName="active" className="nav-link">MY RESERVATIONS</NavLink>
-        <NavLink to="#" activeClassName="active" className="nav-link">ADD MOTORCYCLE</NavLink>
-        <NavLink to="#" activeClassName="active" className="nav-link">RESERVATIONS</NavLink>
+      <div className="d-flex flex-column center-align">
+        <NavLink to="#" exact activeClassName="active" className="nav-link p-3">MOTORSCYCLES</NavLink>
+        <NavLink to="#" activeClassName="active" className="nav-link p-3">MY RESERVATIONS</NavLink>
+        <NavLink to="#" activeClassName="active" className="nav-link p-3">ADD MOTORCYCLE</NavLink>
+        <NavLink to="#" activeClassName="active" className="nav-link p-3">RESERVATIONS</NavLink>
       </div>
     </div>
     <div className="d-flex flex-column align-items-center justify-content-center ">
       <p className="social-wraper">
-      <a href="https://wwww.twitter.com" className="twitter social text-black-50 px-1">
-          <FontAwesomeIcon icon={faTwitter} fontSize='23px' />
+        <a href="https://wwww.twitter.com" className="twitter social text-black-50 px-1">
+          <FontAwesomeIcon icon={faTwitter} fontSize="23px" />
         </a>
         <a
           href="https://www.facebook.com"
           className="facebook social text-black-50 px-1"
         >
-          <FontAwesomeIcon icon={faFacebook} fontSize='23px' />
+          <FontAwesomeIcon icon={faFacebook} fontSize="23px" />
         </a>
         <a
           href="https://www.google.com"
           className="googleplus social text-black-50 px-1"
         >
-          <FontAwesomeIcon icon={faGooglePlus} fontSize='23px' />
+          <FontAwesomeIcon icon={faGooglePlus} fontSize="23px" />
         </a>
         <a
           href="https://www.youtube.com"
           className="youtube social text-black-50 px-1"
         >
-          <FontAwesomeIcon icon={faYoutube} fontSize='23px' />
+          <FontAwesomeIcon icon={faYoutube} fontSize="23px" />
         </a>
-       
+
         <a
           href="http://www.instagram.com"
           className="instagram social text-black-50 px-1"
         >
-          <FontAwesomeIcon icon={faInstagram} fontSize='23px' />
+          <FontAwesomeIcon icon={faInstagram} fontSize="23px" />
         </a>
       </p>
       <div> &copy; 2022 Power Motors Inc.</div>
-      </div>
+    </div>
   </nav>
 );
 
