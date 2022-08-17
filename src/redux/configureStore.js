3e9976b53ec40667a4a59546af39fdbc5877f393
AyoubMS <chahayb563@gmail.com>
2022-08-17 11:26:16 +0100

    fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import motorcylesReducer from './motorcycles/motorcycles';
-import signUpReducer from './session/reducer/signUpReducer';
+import authReducer from './auth';
 import reservationsReducer from './reservations/reservations';
 
 const reducer = combineReducers({
   motorcycle: motorcylesReducer,
-  signUpReducer,
+  auth: authReducer,
   reservations: reservationsReducer,
 });
 
