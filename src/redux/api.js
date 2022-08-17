e60da65d58887d28261cac99c4e7dfb62257bad9
debas-31 <debas31@gmail.com>
2022-08-17 09:39:35 +0300

    Fix linter parsing error

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000/api';
 
-const API = () => {
+const API = {
   login: (user, success) => {
     axios.post(`${baseURL}/users/sign_in`,
       {
@@ -15,7 +15,6 @@ const API = () => {
         success(error.message);
       });
   },
-
   register: (user, success) => {
     axios.post(`${baseURL}/users`,
       {
@@ -77,39 +76,8 @@ const API = () => {
         success(error.message);
       });
   },
-  fetchSingleReservation: (id, userId, success) => {
-    axios.get(`${baseURL}/reservations/${id}?user_id=${userId}`)
-      .then((response) => {
-        success(response);
-      })
-      .catch((error) => {
-        success(error.message);
-      });
-  },
-  addReservation: (userId, reservation, success) => {
-    axios.post(`${baseURL}/reservations?user_id=${userId}`, {
-      reservations: reservation,
-    })
-      .then((response) => {
-        success(response);
-      })
-      .catch((error) => {
-        success(error.message);
-      });
-  },
-  updateReservation: (id, userId, reservation, success) => {
-    axios.patch(`${baseURL}/reservations/${id}?user_id=${userId}`, {
-      reservations: reservation,
-    })
-      .then((response) => {
-        success(response);
-      })
-      .catch((error) => {
-        success(error.message);
-      });
-  },
-  deleteReservation: (id, userId, success) => {
-    axios.delete(`${baseURL}/reservations/${id}?user_id=${userId}`)
+  fetchReservations: (userId, success) => {
+    axios.get(`${baseURL}/reservations?user_id=${userId}`)
       .then((response) => {
         success(response);
       })
@@ -156,7 +124,7 @@ const API = () => {
       .catch((error) => {
         success(error.message);
       });
-  };
+  },
 };
 
 export default API;
