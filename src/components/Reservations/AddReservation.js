d62e66e1a517dfde64d9934ba9fc05772e573bff
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-08-21 15:19:15 +0300

    Handle on submit created

--- src/components/Reservations/AddReservation.js
+++ src/components/Reservations/AddReservation.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import './AddReservation.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import  { faXmark } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import React from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import './AddReservation.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { createReservation } from '../../redux/reservations/reservations';
 const AddReservation = () => {
+  const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(true)
   const close = () => {
     setIsOpen(false) 
     console.log(isOpen)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createReservation());
   }
   if (isOpen) {
   return (
@@ -22,10 +29,10 @@ const AddReservation = () => {
         They are passionate individuals in search of a completely immersive riding experience. 
         Every Power Motorcycle is built from an obsession for innovative design, engineering excellence and 
         dedication to the rider.</p>
-        <div className='d-flex justify-content-center w-50 mx-auto'>
-        <input type={'text'} className='form-control w-25' placeholder='City ' />
+        <form className='d-flex justify-content-center w-50 mx-auto' onSubmit={handleSubmit}>
+        <input type={'text'} className='form-control w-25' placeholder='City ' required />
         <button className='reserve-btn rounded-pill' type='button'>Reserve</button>
-        </div>
+        </form>
         </div>
     </div>
     </div>
