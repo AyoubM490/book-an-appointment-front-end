faf7ca91dc37878c82cb280ace00ab26da3af703
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-08-21 14:41:17 +0300

    close icon , alignment to text added

--- src/components/Reservations/AddReservation.js
+++ src/components/Reservations/AddReservation.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import './AddReservation.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import  { faXmark } from '@fortawesome/free-solid-svg-icons'
 const AddReservation = () => {
   return (
     <div className="add-reservation-main-wrap w-100 h-100 mh-100">
     <div className='add-reservation h-100'>
-      <h2 className='text-center  text-light'>Reserve Motorcycle</h2>
-      <p className='w-25 text-center text-light mt-5'>Power Motorcycle was founded in 2022 to challenge the perception of what an American motorcycle could be. 
+    <FontAwesomeIcon icon={faXmark} className="text-light mx-5 h1 " />
+      <div className='reserve'>
+      <h2 className='text-center text-light'>Reserve Motorcycle</h2>
+      <p className='w-50 text-center text-light mx-auto pb-4'>Power Motorcycle was founded in 2022 to challenge the perception of what an American motorcycle could be. 
         Anything that compromises the ride, aesthetics, stance or function is treated as an opportunity to innovate 
         and improve the design.Power motors owners seek more than just a machine. 
         They are passionate individuals in search of a completely immersive riding experience. 
         Every Power Motorcycle is built from an obsession for innovative design, engineering excellence and 
         dedication to the rider.</p>
+        <div className='d-flex justify-content-center w-50 mx-auto'>
         <input type={'text'} className='form-control w-25' placeholder='City ' />
         <button className='button' type='button'>Reserve</button>
-     
+        </div>
+        </div>
     </div>
     </div>
   )
