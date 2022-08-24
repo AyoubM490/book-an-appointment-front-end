6f8c0253dd4196884a754472275d9929a4616e53
debas-31 <debas31@gmail.com>
2022-08-24 10:53:06 +0300

    Remove navigation from homepage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/signup/SignUp';
 import SignInPage from './pages/signin/SignIn';
-import Navigation from './layout/navigation/Sidebar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-      <Navigation />
       <Routes>
         <Route path="/" element={(<HomePage />)} />
         <Route path="/signin" element={(<SignInPage />)} />
