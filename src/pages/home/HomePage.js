28594e2878aa586c15a5edbca29863cdf02bf4ed
debas-31 <debas31@gmail.com>
2022-08-24 17:00:40 +0300

    Add style to the home for register and login

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css';
 
 const HomePage = () => (
 
   <>
-    <div className="container d-flex flex-column justify-content-center align-items-center mt-5 ">
-      <div className="col ">
-        <Link to="/signin"><button type="button" className="btn btn-success m-2">LOGIN</button></Link>
-      </div>
-      <div className="col ">
-        <Link to="/signup"><button type="button" className="btn btn-success m-2">SIGNUP</button></Link>
+    <div className="home-img">
+      <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+        <h1 className="log-text">BOOK YOUR POWER MOTORCYCLE EASILY</h1>
+        <Link to="/signin"><button type="button" className="btn btn-success px-5 mb-2">LOGIN</button></Link>
+        <Link to="/signup"><button type="button" className="btn btn-outline-success px-5 my-2">SIGNUP</button></Link>
       </div>
     </div>
   </>
