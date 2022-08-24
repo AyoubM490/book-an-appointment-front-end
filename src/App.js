93aff2913e602ffba69fee539881b844fcac3bc9
debas-31 <debas31@gmail.com>
2022-08-24 23:13:35 +0300

    Modify the route in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/signup/SignUp';
@@ -10,10 +10,15 @@ import MyReservationsPage from './components/reservations/MyReservationsPage';
 function App() {
   const currentUser = useSelector((state) => state.auth);
 
+  const location = useLocation();
+  const { pathname } = location;
+
   return (
     <div className="App d-flex">
+      { (pathname.indexOf('sign') !== 1 && pathname.length !== 1) || (pathname.indexOf('sign') !== 1 && pathname.endsWith('/') === false)
+        ? <Navigation />
+        : ''}
       <Routes>
-        <Route path="/home" element={(<Navigation />)} />
         <Route path="/" element={(<HomePage />)} />
         <Route path="/signin" element={(<SignInPage />)} />
         <Route path="/signup" element={(<SignUpPage />)} />
