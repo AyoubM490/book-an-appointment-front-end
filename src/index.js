08fff8c79755871489a34de8dcc90cc5fbe61c27
debas-31 <debas31@gmail.com>
2022-08-25 13:37:54 +0300

    Add BrowserRouter to the index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
-import { login, signup } from './redux/auth';
-
-const user = {
-  name: 'Solomon',
-};
-
-store.dispatch(signup(user));
-
-store.dispatch(login(user));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 );
