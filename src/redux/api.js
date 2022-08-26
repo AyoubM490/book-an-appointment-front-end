81cc5e1355ca710adb9a4c24060ef58517bf3e79
AyoubMS <chahayb563@gmail.com>
2022-08-26 13:25:38 +0100

    add api url

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3000/api';
+const baseURL = 'https://power-motorcycles-api.herokuapp.com/api';
 
 const API = {
   login: (user, success) => {
