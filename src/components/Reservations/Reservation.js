fd4b6cc73d791462a8b414d64d2c93b49bba9095
AyoubMS <chahayb563@gmail.com>
2022-09-15 09:51:20 +0100

    add Reservation test

--- src/components/Reservations/Reservation.js
+++ src/components/Reservations/Reservation.js
@@ -3,21 +3,34 @@ import PropTypes from 'prop-types';
 
 const Reservation = (props) => {
   const { reservation, motorcycles } = props;
-  const motorcycle = motorcycles.length > 0
-    && motorcycles.find((motor) => motor.id === reservation.motorcycle_id);
+  const motorcycle =
+    motorcycles.length > 0 &&
+    motorcycles.find((motor) => motor.id === reservation.motorcycle_id);
 
   return (
-    <tr>
-      <td>{motorcycle.model}</td>
-      <td>{reservation.date}</td>
-      <td>{reservation.city}</td>
-    </tr>
+    <div data-test="reservationComponent">
+      <tr data-test="reservationRow">
+        <td data-test="reservationMotorcycle">{motorcycle.model}</td>
+        <td data-test="reservationDate">{reservation.date}</td>
+        <td data-test="reservationCity">{reservation.city}</td>
+      </tr>
+    </div>
   );
 };
 
 Reservation.propTypes = {
-  reservation: PropTypes.arrayOf.isRequired,
-  motorcycles: PropTypes.arrayOf.isRequired,
+  reservation: PropTypes.shape({
+    id: PropTypes.number,
+    date: PropTypes.string,
+    city: PropTypes.string,
+    motorcycle_id: PropTypes.number,
+  }).isRequired,
+  motorcycles: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      model: PropTypes.string,
+    })
+  ).isRequired,
 };
 
 export default Reservation;
